7c3c5a609375aa9d71a1e69dd1b8ae46de7c8a21
FlavioSantos2002 <luizflavio.20052002@gmail.com>
2023-04-13 18:11:38 -0400

    add

---
+++ lista_AED2/q8.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void entrada(float *a){
+    printf("digite a velocidade me km/h: ");
+    scanf("%f", a);
+}
+
+float processamento(float a){
+    return a/3.6;
+}
+
+void saida(float saida){
+    printf("\no resultado eh: %.2f m/s", saida);
+}
+
+int main(){
+    questao08();
+    return 0;
+}
+
+int questao08(){
+    float a, r;
+    entrada(&a);
+    r = processamento(a);
+    saida(r);
+    return 0;
+}
